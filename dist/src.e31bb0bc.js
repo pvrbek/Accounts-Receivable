390f4dc29cca96cbad66b8d214c58d1ac38450aa
Petar Vrbek <petar.vrbek@perpetuum.eu>
2020-10-04 23:22:00 +0200

    HTML+CSS + JS changes on: color styles, buttons, input types, pagination, filter, list and utilities + filling data with JS

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -125,9 +125,12 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.openFilter = openFilter;
 exports.editData = editData;
+exports.getStatus = getStatus;
+exports.saveData = saveData;
+exports.fillData = fillData;
 
 function openFilter() {
-  document.getElementById("more-button").addEventListener("click", function () {
+  document.getElementById('more-button').addEventListener('click', function () {
     document.querySelector('.filter__secondary').classList.toggle('open');
     var button = document.querySelector('.more-button');
     button.classList.toggle('open');
@@ -136,15 +139,15 @@ function openFilter() {
 
 function editData() {
   var edit = document.querySelectorAll('.menu-box .edit');
-  var list = document.getElementsByClassName('.data');
   var parent;
   var allData;
   var checkOut;
 
   for (var i = 0; i < edit.length; i++) {
     edit[i].addEventListener("click", function (e) {
+      document.getElementsByClassName('btn save')[0].disabled = false;
       parent = e.target.parentElement.parentElement.parentElement;
-      allData = parent.querySelectorAll('.data p');
+      allData = parent.querySelectorAll('.data p.info');
       checkOut = parent.querySelectorAll('.label-options');
       checkOut[0].click();
 
@@ -156,13 +159,57 @@ function editData() {
     });
   }
 }
+
+function saveData() {
+  var allData = document.querySelectorAll('.data p.info');
+  document.getElementsByClassName('btn save')[0].addEventListener("click", function () {
+    document.getElementsByClassName('btn save')[0].disabled = true;
+
+    for (var j = 0; j < allData.length; j++) {
+      allData[j].setAttribute("contentEditable", "false");
+    }
+  });
+}
+
+function getStatus() {
+  var status = document.querySelectorAll('.list-row .ispaid');
+
+  for (var i = 0; i < status.length; i++) {
+    if (status[i].textContent === 'Paid') {
+      status[i].parentElement.parentElement.getElementsByClassName('status')[0].classList.add('paid');
+    } else {
+      status[i].parentElement.parentElement.getElementsByClassName('status')[0].classList.add('late');
+    }
+  }
+}
+
+function createmyElements() {
+  var list = [];
+
+  for (var i = 0; i <= 25; i++) {
+    if (i < 5) {
+      list.push('<li class="list-row box-shadow-small">\n' + '                <div class="status">\n' + '                </div>\n' + '                <div class="data">\n' + '                    <p class="info ispaid" contenteditable="false">Late</p>\n' + '                </div>\n' + '                <div class="data">\n' + '                    <p class="info" contenteditable="false">123456</p>\n' + '                </div>\n' + '                <div class="data">\n' + '                    <p class="info" contenteditable="false">IB Croatia</p>\n' + '                </div>\n' + '                <div class="data">\n' + '                    <p class="info" contenteditable="false">HT</p>\n' + '                </div>\n' + '                <div class="data">\n' + '                    <p class="info" contenteditable="false">SMS</p>\n' + '                </div>\n' + '                <div class="data">\n' + '                    <p class="info" contenteditable="false">100</p>\n' + '                </div>\n' + '                <div class="data">\n' + '                    <p class="info" contenteditable="false">HRK</p>\n' + '                </div>\n' + '                <div class="data">\n' + '                    <p class="info" contenteditable="false">Invoice</p>\n' + '                </div>\n' + '                <div class="data">\n' + '                    <p class="info" contenteditable="false">ZABA</p>\n' + '                </div>\n' + '                <div class="data">\n' + '                    <p class="info" contenteditable="false">10.09.2020.</p>\n' + '                </div>\n' + '                <div class="data">\n' + '                    <p class="info" contenteditable="false">15-30</p>\n' + '                </div>\n' + '                <div class="data menu">\n' + '                    <label class="label-options" for="options' + i + '">' + '                        <div class="icon-container"></div>\n' + '                    </label>\n' + '                    <input type="checkbox" class="options" id="options' + i + '">' + '                    <div class="menu-box box-shadow">\n' + '                        <p class="edit">Edit</p>\n' + '                        <a class="details">Details</a>\n' + '                    </div>\n' + '                </div>\n' + '            </li>');
+    } else {
+      list.push('<li class="list-row box-shadow-small">\n' + '                <div class="status">\n' + '                </div>\n' + '                <div class="data">\n' + '                    <p class="info ispaid" contenteditable="false">Paid</p>\n' + '                </div>\n' + '                <div class="data">\n' + '                    <p class="info" contenteditable="false">123456</p>\n' + '                </div>\n' + '                <div class="data">\n' + '                    <p class="info" contenteditable="false">IB Croatia</p>\n' + '                </div>\n' + '                <div class="data">\n' + '                    <p class="info" contenteditable="false">HT</p>\n' + '                </div>\n' + '                <div class="data">\n' + '                    <p class="info" contenteditable="false">SMS</p>\n' + '                </div>\n' + '                <div class="data">\n' + '                    <p class="info" contenteditable="false">100</p>\n' + '                </div>\n' + '                <div class="data">\n' + '                    <p class="info" contenteditable="false">HRK</p>\n' + '                </div>\n' + '                <div class="data">\n' + '                    <p class="info" contenteditable="false">Invoice</p>\n' + '                </div>\n' + '                <div class="data">\n' + '                    <p class="info" contenteditable="false">ZABA</p>\n' + '                </div>\n' + '                <div class="data">\n' + '                    <p class="info" contenteditable="false">10.09.2020.</p>\n' + '                </div>\n' + '                <div class="data">\n' + '                    <p class="info" contenteditable="false">15-30</p>\n' + '                </div>\n' + '                <div class="data menu">\n' + '                    <label class="label-options" for="options">\n' + '                        <div class="icon-container"></div>\n' + '                    </label>\n' + '                    <input type="checkbox" class="options" id="options">\n' + '                    <div class="menu-box box-shadow">\n' + '                        <p class="edit">Edit</p>\n' + '                        <a class="details">Details</a>\n' + '                    </div>\n' + '                </div>\n' + '            </li>');
+    }
+  }
+
+  return list.join("");
+}
+
+function fillData() {
+  document.getElementsByClassName('list-data')[0].innerHTML = createmyElements();
+}
 },{}],"index.js":[function(require,module,exports) {
 "use strict";
 
 var _main = require("./main");
 
+(0, _main.fillData)();
 (0, _main.openFilter)();
 (0, _main.editData)();
+(0, _main.getStatus)();
+(0, _main.saveData)();
 },{"./main":"main.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -191,7 +238,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54168" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51056" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
